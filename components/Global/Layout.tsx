fa3b1cf60a8125e27029895b744ea3d8fd740293
Bhoomi-Pipalia <bhoomipipalia.15@gmail.com>
2023-10-04 19:51:03 -0400

    Adding style to layout component

--- components/Global/Layout.tsx
+++ components/Global/Layout.tsx
@@ -10,7 +10,9 @@ const Layout = (props: Props) => {
     <>
       <div className="bg-gray-200 dark:bg-gray-900 px-[1rem] py-[2rem] overflow-hidden h-screen">
         <ThemeMode/>
-        { props.children }
+        <div className="flex flex-col gap-[1rem] bg-white dark:bg-black border border-neutral-300 dark:border-neutral-800 shadow-lg rounded-[0.8rem] p-[2rem] overflow-scroll h-[calc(100%-40px)] md:w-[37.5rem] md:max-w-full md:mx-auto lg:h-full">
+          { props.children }
+        </div>
       </div>
     </>
   )
