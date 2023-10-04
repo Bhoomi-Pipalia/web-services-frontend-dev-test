03073aa9c2ea4fc985f691e2a2821e6db08794ae
Bhoomi-Pipalia <bhoomipipalia.15@gmail.com>
2023-10-04 19:52:09 -0400

    Adding style to Card component

--- components/Characters/Card.tsx
+++ components/Characters/Card.tsx
@@ -7,40 +7,55 @@ type Props = {
 
 const Card = ({ character } : Props) => {
 
+  let textColorClass = 'text-gray-700 dark:text-gray-400';
+  if ( character?.biography?.alignment == 'bad' ) {
+    textColorClass = 'text-red-700';
+  } else if ( character?.biography?.alignment == 'good' ) {
+    textColorClass = 'text-blue-700 dark:text-white';
+  }
+
   return (
-    <>
+    <div className="relative flex flex-col border border-neutral-300 dark:border-neutral-800 rounded-[0.8rem] overflow-hidden shadow-lg md:flex-row">
+
       {/* Thumbnail */}
-      <Image
-        src={character?.images?.lg}
-        alt={character.slug}
-        width={200}
-        height={200}
-        className=""
-      />
+      <div className="basis-[12.5rem] grow-1 shrink-0 w-full h-[10rem] md:w-[12.5rem] md:h-[unset]">
+        <Image
+          src={character?.images?.lg}
+          alt={character.slug}
+          width={578}
+          height={300}
+          className="object-cover object-center w-full h-full"
+        />
+      </div>
 
       {/* Details */}
-      <h2>{ character.name }</h2>
-      {
-        character.biography.fullName ?
-          <p>Full Name: {character.biography.fullName}</p>
-        : <></>
-      }
-      {
-        character.appearance.race ?
-          <p>Race: {character.appearance?.race}</p>
-        : <></>
-      }
-      {
-        character.biography.alignment ?
-          <p>Alignment: {character.biography.alignment}</p>
-        : <></>
-      }
-      {
-        character.biography.publisher ?
-          <p>Publisher: {character.biography.publisher}</p>
-        : <></>
-      }
-    </>
+      <div className={`grow flex flex-col gap-[1.5rem] p-[2rem] ${textColorClass}`}>
+
+        <div>
+          <h2>{ character.name }</h2>
+          {
+            character.biography.fullName ?
+              <p>Full Name: <span className="capitalize">{character.biography.fullName}</span></p>
+            : <></>
+          }
+          {
+            character.appearance.race ?
+              <p>Race: <span className="capitalize">{character.appearance?.race}</span></p>
+            : <></>
+          }
+          {
+            character.biography.alignment ?
+              <p>Alignment: <span className="capitalize">{character.biography.alignment}</span></p>
+            : <></>
+          }
+          {
+            character.biography.publisher ?
+              <p>Publisher: <span className="capitalize">{character.biography.publisher}</span></p>
+            : <></>
+          }
+        </div>
+      </div>
+    </div>
   );
 };
 
