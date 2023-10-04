2ad01befd80afcab3b350c1581f8d0678fecf804
Bhoomi-Pipalia <bhoomipipalia.15@gmail.com>
2023-10-04 19:39:32 -0400

    Adjusting LocalStorage file with proper comments and indentation

--- utils/LocalStorage.tsx
+++ utils/LocalStorage.tsx
@@ -1,27 +1,37 @@
+/**
+ * Set data to local storage.
+ *
+ * @param {string} key Local storage key.
+ * @param {any} data Data to store.
+ */
 export const setLocalStorage = (key:string, data:any): void => {
   if (typeof window !== "undefined") {
-	  window.localStorage.setItem(key, JSON.stringify(data));
+    window.localStorage.setItem(key, JSON.stringify(data));
   }
 };
 
-
+/**
+ * Get data from local storage.
+ *
+ * @param {string} key Local storage key.
+ */
 export const getLocalStorage = ( key:string ): any | null => {
-	let data = null;
+  let data = null;
 
-	if (typeof window !== "undefined") {
-		try {
-			const storedData: string | null = window.localStorage.getItem(
-				key
-			);
-			if (storedData) {
-				data = JSON.parse(storedData);
-			}
-		} catch (err) {
-			console.error(err);
-			// If stored data is not a stringified JSON this will fail,
-			// that's why we catch the error
-		}
-	}
+  if (typeof window !== "undefined") {
+    try {
+      const storedData: string | null = window.localStorage.getItem(
+        key
+      );
+      if (storedData) {
+        data = JSON.parse(storedData);
+      }
+    } catch (err) {
+      console.error(err);
+      // If stored data is not a stringified JSON this will fail,
+      // that's why we catch the error
+    }
+  }
 
-	return data;
+  return data;
 };
