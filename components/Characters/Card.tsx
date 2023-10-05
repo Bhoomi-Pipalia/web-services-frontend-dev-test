12bdf00fe560784274b1451a9adda803ff9f2328
Bhoomi-Pipalia <bhoomipipalia.15@gmail.com>
2023-10-04 21:14:02 -0400

    Adding card collapse view.

--- components/Characters/Card.tsx
+++ components/Characters/Card.tsx
@@ -1,4 +1,5 @@
 import { ICharacter } from '@/interfaces/character';
+import { useCollapse } from 'react-collapsed';
 import Image from "next/image";
 
 type Props = {
@@ -6,6 +7,8 @@ type Props = {
 };
 
 const Card = ({ character } : Props) => {
+
+  const { getCollapseProps, getToggleProps, isExpanded } = useCollapse();
 
   let textColorClass = 'text-gray-700 dark:text-gray-400';
   if ( character?.biography?.alignment == 'bad' ) {
@@ -54,7 +57,66 @@ const Card = ({ character } : Props) => {
             : <></>
           }
         </div>
+
+        {/* Collapse */}
+        <div className="flex flex-col gap-[1rem]" {...getCollapseProps()}>
+
+          {/* Powers */}
+          <div>
+            <h3>Powers:</h3>
+            <ul role="list">
+              {
+                character.powerstats.intelligence ?
+                  <li>
+                    <span className="capitalize">Intelligence</span> : { character.powerstats.intelligence }
+                  </li>
+                : <></>
+              }
+              {
+                character.powerstats.strength ?
+                  <li>
+                    <span className="capitalize">Strength</span> : { character.powerstats.strength }
+                  </li>
+                : <></>
+              }
+              {
+                character.powerstats.speed ?
+                  <li>
+                    <span className="capitalize">Speed</span> : { character.powerstats.speed }
+                  </li>
+                : <></>
+              }
+              {
+                character.powerstats.durability ?
+                  <li>
+                    <span className="capitalize">Durability</span> : { character.powerstats.durability }
+                  </li>
+                : <></>
+              }
+              {
+                character.powerstats.power ?
+                  <li>
+                    <span className="capitalize">Power</span> : { character.powerstats.power }
+                  </li>
+                : <></>
+              }
+              {
+                character.powerstats.combat ?
+                  <li>
+                    <span className="capitalize">Combat</span> : { character.powerstats.combat }
+                  </li>
+                : <></>
+              }
+            </ul>
+          </div>
+        </div>
+
       </div>
+
+      {/* Button */}
+      <button {...getToggleProps()} aria-expanded={isExpanded ? 'true' : 'false'} className="m-1 w-[2rem] h-[2rem] border border-gray-800 dark:border-white rounded-full text-gray-800 dark:text-white absolute right-[0] top-[12.5rem] md:top-0" aria-label={isExpanded ? 'Collapse' : 'Expand'}>
+        {isExpanded ? '-' : '+'}
+      </button>
     </div>
   );
 };
